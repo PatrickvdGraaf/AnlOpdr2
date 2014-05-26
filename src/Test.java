c7b3258961521923a278a77580d0ab15a419ae8b
Nick <nickgobee0122@gmail.com>
2014-05-26 15:41:14 +0200

    Dirty read(?)

--- src/Test.java
+++ src/Test.java
@@ -1,107 +1,105 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Test { 
+public class Test {
+
     protected static int monitorVoorraad;
     protected static Statement stmt = null;
-	protected static Connection conn = null;
-	protected static ResultSet rs = null;
+    protected static Connection conn = null;
+    protected static ResultSet rs = null;
 
-	public static void main(String[] args) throws NumberFormatException, SQLException { 
+    public static void main(String[] args) throws NumberFormatException, SQLException {
 
-		
-		try {
-		    conn =DriverManager.getConnection("jdbc:mysql://95.85.1.60:3306/opdracht_twee" ,"ANL", "ANL");
-				
-				    stmt = conn.createStatement();
-				    rs = stmt.executeQuery("SELECT * FROM voorraad");
-				
-		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}finally {
-		    if (rs != null) {
-		    	while (rs.next()) {
-		    	    if(rs.getString(2).equals("Monitor")){
-		    	    	monitorVoorraad = Integer.parseInt(rs.getString(3));
-		    	    }
-		    	}
-		    }
-		}
-		 
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://95.85.1.60:3306/opdracht_twee", "ANL", "ANL");
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM voorraad");
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        } finally {
+            if (rs != null) {
+                while (rs.next()) {
+                    if (rs.getString(2).equals("Monitor")) {
+                        monitorVoorraad = Integer.parseInt(rs.getString(3));
+                    }
+                }
+            }
+        }
+
         // Maak en start thread 1 
-        new Thread(new Runnable() { 
-            @Override 
-            public void run() { 
-            	while (true) { 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
                     // Schrijf hier je eigen code 
-                		try {
-                			int situatie = (int) Math.random() * 11; 
-                		
-                			if(situatie >= 5){
-                				int bestelling = (int) (Math.random() * 11);
-                				stmt.executeUpdate("UPDATE voorraad SET amount = amount - " + bestelling + " WHERE name = 'Monitor'");
-    							monitorVoorraad = monitorVoorraad - bestelling;
-    	                        System.out.println("Bestelling van " + bestelling + " producten, totaal nu op " + monitorVoorraad);
-                			}else{
-                				int levering = (int) (Math.random() * 11); 
-                				stmt.executeUpdate("UPDATE voorraad SET amount = amount + " + levering + " WHERE name = 'Monitor'");
-    							monitorVoorraad = monitorVoorraad + levering;
-    							//System.out.println(Thread.currentThread().getName() + ": Tijd tot volgende levering:" + wachtTijd2 + " sec"); 
-    	                        System.out.println("Levering van " + levering + " producten, totaal nu op " + monitorVoorraad);
-                			}
-         				    // Slaap wachtTijd seconden 
-	                        Thread.sleep((int) (Math.random() * 6) * 1000);
+                    try {
+                        double situatie =  Math.random() * 11;
+
+                        if (situatie >= 5) {
+                            int bestelling = (int) (Math.random() * 11);
+                            stmt.executeUpdate("UPDATE voorraad SET amount = amount - " + bestelling + " WHERE name = 'Monitor'");
+                            monitorVoorraad = monitorVoorraad - bestelling;
+                            System.out.println("Bestelling van " + bestelling + " producten, totaal nu op " + monitorVoorraad);
+                        } else {
+                            int levering = (int) (Math.random() * 11);
+                            stmt.executeUpdate("UPDATE voorraad SET amount = amount + " + levering + " WHERE name = 'Monitor'");
+                            monitorVoorraad = monitorVoorraad + levering;
+                            //System.out.println(Thread.currentThread().getName() + ": Tijd tot volgende levering:" + wachtTijd2 + " sec"); 
+                            System.out.println("Levering van " + levering + " producten, totaal nu op " + monitorVoorraad);
+                        }
+                        // Slaap wachtTijd seconden 
+                        Thread.sleep((int) (Math.random() * 6) * 1000);
 	                        //
-	                        
-	                        
-						} catch (SQLException e1) {
-							e1.printStackTrace();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-                } 
-            } 
-        }, "Thread 1").start(); 
- 
+
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "Thread 1").start();
+
         // Maak en start thread 2. Deze draait tegelijkertijd met thread 1 
-        new Thread(new Runnable() { 
-            @Override 
-            public void run() { 
-            	while (true) { 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
                     // Schrijf hier je eigen code 
-                		try {
-                			int situatie = (int) Math.random() * 11;                 			
-         				    // Slaap wachtTijd seconden 
-                			if(situatie >= 5){
-                				int bestelling = (int) (Math.random() * 11);
-                				stmt.executeUpdate("UPDATE voorraad SET amount = amount - " + bestelling + " WHERE name = 'Monitor'");
-    							monitorVoorraad = monitorVoorraad - bestelling;
-    	                        System.out.println("Bestelling van " + bestelling + " producten, totaal nu op " + monitorVoorraad);
-                			}else{
-                				int levering = (int) (Math.random() * 11); 
-                				stmt.executeUpdate("UPDATE voorraad SET amount = amount + " + levering + " WHERE name = 'Monitor'");
-    							monitorVoorraad = monitorVoorraad + levering;
-    							//System.out.println(Thread.currentThread().getName() + ": Tijd tot volgende levering:" + wachtTijd2 + " sec"); 
-    	                        System.out.println("Levering van " + levering + " producten, totaal nu op " + monitorVoorraad);
-                			}
-                			
-	                        Thread.sleep((int) (Math.random() * 6) * 1000);
-	                        
-	                        
-	                        
-						} catch (SQLException e1) {
-							e1.printStackTrace();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-                } 
-            } 
-        }, "Thread 2").start(); 
-    } 
+                    try {
+                        double situatie = Math.random() * 11;
+                        // Slaap wachtTijd seconden 
+                        if (situatie >= 5) {
+                            int bestelling = (int) (Math.random() * 11);
+                            stmt.executeUpdate("UPDATE voorraad SET amount = amount - " + bestelling + " WHERE name = 'Monitor'");
+                            monitorVoorraad = monitorVoorraad - bestelling;
+                            System.out.println("Bestelling van " + bestelling + " producten, totaal nu op " + monitorVoorraad);
+                        } else {
+                            int levering = (int) (Math.random() * 11);
+                            stmt.executeUpdate("UPDATE voorraad SET amount = amount + " + levering + " WHERE name = 'Monitor'");
+                            monitorVoorraad = monitorVoorraad + levering;
+                            //System.out.println(Thread.currentThread().getName() + ": Tijd tot volgende levering:" + wachtTijd2 + " sec"); 
+                            System.out.println("Levering van " + levering + " producten, totaal nu op " + monitorVoorraad);
+                        }
+
+                        Thread.sleep((int) (Math.random() * 6) * 1000);
+
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "Thread 2").start();
+    }
 }
---
+++ src/rollback_test.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Nick
+ */
+public class rollback_test {
+
+    protected static int monitorVoorraad;
+    protected static Statement stmt = null;
+    protected static Connection conn = null;
+    protected static ResultSet rs = null;
+    protected static int current;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://95.85.1.60:3306/opdracht_twee", "ANL", "ANL");
+            conn.setAutoCommit(false);
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM voorraad");
+
+            while (rs.next()) {
+                if (rs.getString(2).equals("Monitor")) {
+                    monitorVoorraad = Integer.parseInt(rs.getString(3));
+                }
+            }
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    // Schrijf hier je eigen code 
+                    try {
+
+                        int bestelling = 10;
+                        stmt.executeUpdate("UPDATE voorraad SET amount = amount - " + bestelling + " WHERE name = 'Monitor'");
+                        rs = stmt.executeQuery("SELECT * FROM voorraad");
+                        while (rs.next()) {
+                            if (rs.getString(2).equals("Monitor")) {
+                                monitorVoorraad = Integer.parseInt(rs.getString(3));
+                            }
+                        }
+                        System.out.println("1)Voor rollback: Bestelling van " + bestelling + " producten, totaal nu op " + monitorVoorraad);
+
+                        // Slaap wachtTijd seconden 
+                        Thread.sleep(4000);
+                        conn.rollback();
+                        rs = stmt.executeQuery("SELECT * FROM voorraad");
+                        while (rs.next()) {
+                            if (rs.getString(2).equals("Monitor")) {
+                                monitorVoorraad = Integer.parseInt(rs.getString(3));
+                            }
+                        }
+                        System.out.println("2)Na rollback: Totaal nu op " + monitorVoorraad);
+
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "Thread 1").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+                    // Schrijf hier je eigen code 
+                    try {
+
+                        Thread.sleep(1000);
+                        int bestelling = 20;
+                        rs = stmt.executeQuery("SELECT * FROM voorraad");
+                        while (rs.next()) {
+                            if (rs.getString(2).equals("Monitor")) {
+                                current = Integer.parseInt(rs.getString(3));
+                            }
+                        }
+                        Thread.sleep(2000);
+                        stmt.executeUpdate("UPDATE voorraad SET amount = " + (current - bestelling) + " WHERE name = 'Monitor'");
+                        rs = stmt.executeQuery("SELECT * FROM voorraad");
+                        while (rs.next()) {
+                            if (rs.getString(2).equals("Monitor")) {
+                                current = Integer.parseInt(rs.getString(3));
+                            }
+                        }
+                        System.out.println("3)Bestelling van " + bestelling + " producten, totaal nu op " + current);
+                        conn.commit();
+
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "Thread 2").start();
+    }
+}
